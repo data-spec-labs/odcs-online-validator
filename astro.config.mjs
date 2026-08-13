7b9b3b6e3d6fc2f6f49388bc1b12bcc300d0f0e2
Rajat Goel <rajatgl17@gmail.com>
2026-08-13 17:54:23 +0530

    Added index now

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import indexnow from 'astro-indexnow';
 
 export default defineConfig({
   site: 'https://odcs-validator.com',
   integrations: [
     react(),
     sitemap(),
+    indexnow({
+      host: 'https://odcs-validator.com',
+      key: '92d80210d05a43389115b51dc03baef0',
+      keyLocation: 'https://odcs-validator.com',
+    })
   ],
   vite: {
     plugins: [tailwindcss()],
